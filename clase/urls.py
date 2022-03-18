e590e96fab540807e2c64941c43e1c2d8d235e24
Florencia <sicref001@gmail.com>
2022-03-18 11:56:53 -0300

    plantilla heredada

--- clase/urls.py
+++ clase/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import Datos, company, salary
+from .views import Datos, company, salary, info_curso
 
 urlpatterns=[
     path('Datos/',Datos, name= 'Datos' ),
     path('company/',company, name= 'company' ),
-    path('salary/',salary, name= 'salario' )
+    path('salary/',salary, name= 'salario' ),
+    path('info/',info_curso, name= 'info' ),
 ]
--- clase/views.py
+++ clase/views.py
@@ -1,20 +1,28 @@
 
 
 # Create your views here.
+from urllib import request
 from django.http import HttpResponse
+from django.shortcuts import render
 from clase.models import datos,empresa,salario
+
 def Datos(self):
     Datos = datos(nombre = 'Flor', profesion = 'est')
     Datos.save()
     texto = f"--> Nombre {Datos.nombre} profesion {Datos.profesion} "
     return HttpResponse(texto)
+
 def company(self):
     Empresa = empresa(empresa = 'google', cantEmpleados = 40000 )
     Empresa.save()
     texto= f"--> Lugar de trabajo: {Empresa.empresa}, compañeros {Empresa.cantEmpleados}"
     return HttpResponse (texto)
+
 def salary(self):
     Salario = salario(salario = '90000', aumento = '0' )
     Salario.save()
     textoSalario = f"--> Salario {Salario.salario}, aumento {Salario.aumento}"
     return HttpResponse (textoSalario)
+
+def info_curso(request):
+    return render(request, 'clase/info_curso.html',{})
